001b8a55cb7946e272957770a59e7e52643dc3ea
Manav <manav.prakash@1mg.com>
2023-06-14 22:11:16 +0530

    Initial commit

---
+++ SwiftData/SwiftData/SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataApp.swift
+//  SwiftData
+//
+//  Created by Manav Prakash on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
